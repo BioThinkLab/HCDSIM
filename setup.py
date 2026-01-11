b7cc9a27668626530e57727c292aa4e6aab8872b
Eric Feng <xikanfeng2@gmail.com>
2026-01-12 04:36:52 +0800

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="HCDSIM",
-    version="1.3.1",
+    version="2.0.0",
     author="Xikang Feng",
     author_email="fxk@nwpu.edu.cn",
     maintainer="Sisi Peng",
